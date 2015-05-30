672a9842d003ccf1813c6aef15bf8ac3df096a52
M42 <mromang08@gmail.com>
2015-05-30 15:09:48 +0200

    Changes the csv file direction

--- ftpdownload.sh
+++ ftpdownload.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 DATAFILE=ftpdata.csv
 
-echo -e "MBs,B" > ftpresults.data
+echo -e "MBs,B" > $DATAFILE
 wget -r ftp://localhost/bach --user=$1 --password=$2 2>&1 | grep saved | cut -f3,8 -d' ' | tr -d '([]' | tr ',' '.' | tr ' ' ',' >> $DATAFILE
