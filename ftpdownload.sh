75ae1b7eaa7f8b7e39fbf72070d950b999ebe0c0
M42 <mromang08@gmail.com>
2015-05-30 15:07:59 +0200

    Arbitrary direction for the csv file

--- ftpdownload.sh
+++ ftpdownload.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+DATAFILE=ftpdata.csv
 
 echo -e "MBs,B" > ftpresults.data
-wget -r ftp://localhost/bach --user=$1 --password=$2 2>&1 | grep saved | cut -f3,8 -d' ' | tr -d '([]' | tr ',' '.' | tr ' ' ',' >> ftpresults.data
+wget -r ftp://localhost/bach --user=$1 --password=$2 2>&1 | grep saved | cut -f3,8 -d' ' | tr -d '([]' | tr ',' '.' | tr ' ' ',' >> $DATAFILE
