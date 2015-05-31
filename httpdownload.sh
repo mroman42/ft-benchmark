671f1fc365780188ef3569d28fb797000b0400e8
M42 <mromang08@gmail.com>
2015-05-31 16:20:29 +0200

    Unifies download scripts

--- httpdownload.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo -e "MBs,B" > $4
-wget -r $1 --user=$2 --password=$3 --no-check-certificate 2>&1 | grep saved | cut -f3,8 -d' ' | tr -d '([]' | tr ',' '.' | tr ' ' ',' | cut -f1 -d'/' >> $4
